18941030e667a8ecd5351649a551277b106a0fda
Aswanth P <Aswanth@NTP-LAP-734.chn.nestgroup.net>
2022-11-21 12:20:54 +0530

    main

---
+++ menu_program.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class menu_program {
+
+    public static void main(String[] args) {
+        int n1,n2,n3;
+        int op;
+        System.out.println("-----------------------------------");
+        System.out.println("enter your option");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("1.Largest\n2.Smallest\n4.prime or note\n4.Even or note\n5.reverse\n6.Exit");
+        op = sc.nextInt();
+
+
+
+
+    }
+}
